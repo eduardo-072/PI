5bec3af02de5ae04119352b1fe3183e0c359bedf
eduardo-072 <141462212+eduardo-072@users.noreply.github.com>
2024-02-16 01:07:21 +0000

    Tarefa realizada com muita dificuldade técnica

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Faça o Calculo da área ");
+
+double Raio;
+double r;
+double PI = 3.14159;
+
+Console.WriteLine("A = 3,14159 * R²");
+Console.Write("A = 3,14159 * ");
+
+Raio  = double.Parse(Console.ReadLine());
+r = Math.Pow(Raio,2);
+
+double multi = PI * r;
+
+Console.WriteLine($"O resultado da conta é esse: {multi}");
